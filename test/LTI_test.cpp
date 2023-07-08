e6eeee00289cfd5031818dbaa2b1d6138aac588e
WANG CHENFEI <barrycf.wang@gmail.com>
2023-07-09 01:10:55 +0800

    Update LTI_test.cpp

--- test/LTI_test.cpp
+++ test/LTI_test.cpp
@@ -26,3 +26,32 @@ TEST_CASE("State Space Test : Second Order Integrator"){
 
 
 }
+
+TEST_CASE("State Space Test : Linear Check"){
+    using namespace LTI;
+    StateSpace<2, 1, 1> ss;
+    ss.A << -1, -2,
+            -3, -4;
+    ss.B << -1, 1;
+    ss.C << 1, 0;
+
+    ss.x(0) = 0;
+
+    auto ss2 = ss;
+    auto ss3 = ss;
+
+    for (; ss.t < 5; )
+    {
+        ss.u(0) = sin(ss.t);
+        ss2.u(0) = cos(ss2.t);
+        ss3.u(0) = sin(ss.t) + 2 * cos(ss2.t);
+
+        ss.step(1E-3);
+        ss2.step(1E-3);
+        ss3.step(1E-3);
+    }
+    CHECK(ss.y(0)+ss2.y(0)*2 == doctest::Approx(ss3.y(0)).epsilon(1E-3));    // Check linearity
+
+//    std::cout << ss.y(0)+ss2.y(0)*2 << " |||| "  << ss3.y(0) << std::endl;
+
+}
