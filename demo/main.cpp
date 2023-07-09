b989c498306e4caa8ea8f3f9004dfc8b75e35507
WANG CHENFEI <barrycf.wang@gmail.com>
2023-07-09 11:14:53 +0800

    separate LTI to other repo

--- demo/main.cpp
+++ demo/main.cpp
@@ -84,67 +84,6 @@ void testCCD()
 }
 
 
-#include "CHEN/ODEsolver.hpp"
-
-void testODE()
-{
-	using namespace ODEsolver;
-
-	State<1> y;
-	State<1> u;
-	double t = 0;
-	y << 1;
-	u << 0;
-//	Model::FirstOrderIntegrator()
-	auto handle = Model::FirstOrderIntegratorWithKb;
-
-	for (int k = 0; k<10; ++k)
-	{
-		u(0) = -y(0);
-		y = RK4<1, 1, double, double>(y, u, t, 1E-1, handle, 1, 1);
-		t += 1E-3;
-		cout << t << ' ' << y(0) << endl;
-	}
-	cout << " ============================== " << endl;
-
-	State<2> y2;
-	State<1> u2;
-	double t2 = 0;
-	y2 << 1, 0;
-	u2 << 0;
-	t2 = 0;
-	auto handle2 = Model::SecondOrderIntegrator;
-	for (int k = 0; k<10; ++k)
-	{
-		u2(0) = -y2(0);
-		y2 = RK4_doublestep<2, 1>(y2, u2, t2, 1E-1, handle2);
-		t2 += 1E-3;
-		cout << t2 << ' ' << y2(0) << endl;
-	}
-}
-
-#include "CHEN/statespace.hpp"
-void testControl()
-{
-	using namespace LTI;
-
-	StateSpace<2, 1, 1> ss;
-
-	// second order integrater
-	ss.A << 0.1, 1.1,
-		-0.1, 2;
-	ss.B << -1, 1;
-	ss.C << 1, 0;
-
-	ss.u(0) = 1;
-	for (int k = 0; k < 1000; ++k)
-	{
-		ss.step(1E-3);
-		cout << " t : " << ss.t << " x : " << ss.x.transpose() << endl;
-	}
-
-}
-
 #include "array"
 int main()
 {
