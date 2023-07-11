66bc3aa01aad997bbc41860a7405178b5dd13bd7
WANG CHENFEI <barrycf.wang@gmail.com>
2023-07-11 09:43:37 +0800

    repath to solving adrress

---
+++ Include_temp/NCCD/quat.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Administrator on 2023/7/11.
+//
+
+#ifndef TCCPPLIB_INCLUDE_NCCD_QUAT_HPP_
+#define TCCPPLIB_INCLUDE_NCCD_QUAT_HPP_
+#include "vec.hpp"
+namespace NCCD
+{
+	using quat_t = Eigen::Quaterniond;
+
+
+}
+
+#endif //TCCPPLIB_INCLUDE_NCCD_QUAT_HPP_
